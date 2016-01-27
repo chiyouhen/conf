0beff5acdffe7aa9a313132ec2217d4b67910777
chiyouhen <chiyouhen@gmail.com>
2016-01-27 18:12:04 +0800

    split conf engeine

--- conf.go
+++ conf.go
@@ -1,25 +1,14 @@
 package conf
-//
+
 import (
-    "os"
     "io"
-    "io/ioutil"
-    "bytes"
-    "bufio"
     "strings"
-    "regexp"
     "path/filepath"
     "strconv"
     "time"
-    "fmt"
 )
 
 var (
-    commentString = map[string]bool{
-        "#": true,
-        ";": true,
-    }
-    validKeyPattern, _ = regexp.Compile("^[A-Za-z][A-Za-z0-9-_]*$")
     boolMap = map[string]bool{
         "on": true,
         "off": false,
@@ -30,17 +19,8 @@ var (
     }
 )
 
-type ConfError struct {
-    msg string
-    filename string
-    lineno int
-}
-
-func (e *ConfError) Error() string {
-    return fmt.Sprintf("%s in '%s' line %d", e.msg, e.filename, e.lineno)
-}
-
 type Conf struct {
+    Handler SyntaxHandler
     confRoot string
     prefix string
     data interface{}
@@ -48,227 +28,42 @@ type Conf struct {
     items [][]interface{}
 }
 
-func ReadData(rd io.Reader) (data map[string]interface{}, err error) {
-    err = nil
-    data = make(map[string]interface{})
-    var sc = bufio.NewScanner(rd)
-    var l string;
-    var s string;
-    var g []string;
-    var stack = make([]map[string]interface{}, 0, 512)
-    var curr = data
-    var ok bool
-    var lg int
-    var lv int
-    var k string
-    var v interface{}
-    var fp *os.File
-
-    var lineno = 0
-    var filename = "<io.Reader>"
-
-    fp, ok = rd.(*os.File)
-    if ok {
-        filename = fp.Name()
-    }
-
-    stack = append(stack, curr)
-    for sc.Scan() {
-        lineno ++
-        l = sc.Text()
-        l = strings.TrimSpace(l)
-        if len(l) == 0 {
-            continue
-        }
-
-        // comment
-        if _, ok = commentString[string(l[0])]; ok {
-            continue
-        }
-
-        // for group
-        if l[0] == '[' && l[len(l) - 1] == ']' {
-            s = strings.Trim(l, "[]")
-            g = strings.Split(s, ".")
-            curr = data
-            lg = len(g)
-            for lv, k = range g {
-                if k == "" {
-                    // we get . here
-                    curr = stack[lv + 1]
-                } else {
-                    // . but with a string key
-                    if _, ok = curr[k]; ok {
-                        curr = curr[k].(map[string]interface{})
-                        stack = stack[0:lv + 1]
-                        stack = append(stack, curr)
-                    }
-                }
-            }
-            stack = stack[0:lg]
-            k = g[lg - 1]
-            v = make(map[string]interface{})
-            // check tail key
-            if k[0] == '@' {
-                // array here
-                k = string(k[1:])
-                if ! validKeyPattern.Match([]byte(k)) {
-                    err = &ConfError{"invalid key", filename, lineno}
-                    return
-                }
-                if _, ok = curr[k]; ! ok {
-                    curr[k] = make([]interface{}, 0, 0)
-                }
-                curr[k] = append(curr[k].([]interface{}), v)
-            } else {
-                // map here
-                if ! validKeyPattern.Match([]byte(k)) {
-                    err = &ConfError{"invalid key", filename, lineno}
-                    return
-                }
-                if _, ok = curr[k]; ok {
-                    err = &ConfError{"duplicate key", filename, lineno}
-                    return
-                }
-                curr[k] = v
-            }
-            curr = v.(map[string]interface{})
-            stack = append(stack, curr)
-        } else {
-            g = strings.SplitN(l, ":", 2)
-            if len(g) != 2 {
-                err = &ConfError{"syntax error", filename, lineno}
-                return
-            }
-            k = strings.TrimSpace(g[0])
-            v = strings.TrimSpace(g[1])
-            if k[0] == '@' {
-                k = string(k[1:])
-                if ! validKeyPattern.Match([]byte(k)) {
-                    err = &ConfError{"invalid key", filename, lineno}
-                    return
-                }
-                if _, ok = curr[k]; ! ok {
-                    curr[k] = make([]interface{}, 0, 0)
-                }
-                curr[k] = append(curr[k].([]interface{}), v)
-            } else {
-                if ! validKeyPattern.Match([]byte(k)) {
-                    err = &ConfError{"invalid key", filename, lineno}
-                    return
-                }
-                curr[k] = v
-            }
-        }
-    }
-    return
-}
-
-func ParseData(b []byte) (data map[string]interface{}, err error) {
-    var buf = bytes.NewBuffer(b)
-    return ReadData(buf)
-}
-
-func LoadFileData(filename string) (data map[string]interface{}, err error) {
-    var rd *os.File
-    rd, err = os.OpenFile(filename, os.O_RDONLY, 0644)
-    if err != nil {
-        return
-    }
-    defer rd.Close()
-    data, err = ReadData(rd)
-    return
-}
-
-func LoadDirData(dir string) (data map[string]interface{}, err error) {
-    err = nil
-    data = make(map[string]interface{})
-    var dirs = make([]os.FileInfo, 0, 512)
-    var files = make([]os.FileInfo, 0, 512)
-    var dirContent []os.FileInfo
-    var hasGlobal = false
-    var f os.FileInfo
-    var k string
-    var ok bool
-
-    dirContent, err = ioutil.ReadDir(dir)
-    if err != nil {
-        return
-    }
-    for _, f = range dirContent {
-        if f.Name()[0] == '.' {
-            continue
-        } else if f.IsDir() {
-            dirs = append(dirs, f)
-        } else if f.Name() == "_global.conf" {
-            hasGlobal = true
-        } else if filepath.Ext(f.Name()) == ".conf" {
-            files = append(files, f)
-        }
-    }
-    if hasGlobal {
-        data, err = LoadFileData(filepath.Join(dir, "_global.conf"))
-        if err != nil {
-            return
-        }
-    }
-    for _, f = range files {
-        k = strings.TrimSuffix(f.Name(), ".conf")
-        if _, ok = data[k]; ! ok {
-            data[k], err = LoadFileData(filepath.Join(dir, f.Name()))
-            if err != nil {
-                return
-            }
-        }
-    }
-    for _, f = range dirs {
-        k = f.Name()
-        if _, ok = data[k]; ! ok {
-            data[k], err = LoadDirData(filepath.Join(dir, f.Name()))
-            if err != nil {
-                return
-            }
-        }
-    }
-    return
-}
-
-func Read(rd io.Reader) (cf *Conf, err error) {
-    cf = &Conf{}
+func Read(h SyntaxHandler, rd io.Reader) (cf *Conf, err error) {
+    cf = &Conf{Handler: h}
     err = cf.Read(rd)
     return
 }
 
-func Parse(b []byte) (cf *Conf, err error) {
-    cf = &Conf{}
+func Parse(h SyntaxHandler, b []byte) (cf *Conf, err error) {
+    cf = &Conf{Handler: h}
     err = cf.Parse(b)
     return
 }
 
-func LoadFile(filename string) (cf *Conf, err error) {
-    cf = &Conf{}
-    err = cf.LoadFile(filename)
+func Load(h SyntaxHandler, filename string) (cf *Conf, err error) {
+    cf = &Conf{Handler: h}
+    err = cf.Load(filename)
     return
 }
 
-func LoadDir(confRoot string) (cf *Conf, err error) {
-    cf = &Conf{}
+func LoadDir(h SyntaxHandler, confRoot string) (cf *Conf, err error) {
+    cf = &Conf{Handler: h}
     err = cf.LoadDir(confRoot)
     return
 }
 
 func (cf *Conf) Read(rd io.Reader) (err error) {
-    cf.data, err = ReadData(rd)
+    cf.data, err = cf.Handler.Read(rd)
     return
 }
 
 func (cf *Conf) Parse(b []byte) (err error) {
-    cf.data, err = ParseData(b)
+    cf.data, err = cf.Handler.Parse(b)
     return
 }
 
-func (cf *Conf) LoadFile(filename string) (err error) {
-    cf.data, err = LoadFileData(filename)
+func (cf *Conf) Load(filename string) (err error) {
+    cf.data, err = cf.Handler.Load(filename)
     if err != nil {
         cf.confRoot = filepath.Dir(filename)
     }
@@ -276,7 +71,7 @@ func (cf *Conf) LoadFile(filename string) (err error) {
 }
 
 func (cf *Conf) LoadDir(confRoot string) (err error) {
-    cf.data, err = LoadDirData(confRoot)
+    cf.data, err = cf.Handler.LoadDir(confRoot)
     cf.confRoot = confRoot
     return
 }
